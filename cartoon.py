8b38485e61392413dc4b17c86b0bcb9f55eaf19e
Exousia KASEKA <kasekaexousia@gmail.com>
2024-03-21 11:47:26 +0100

    Created Cartoon class for managing turtle graphics operations and Implemented __init__ method to initialize turtle settings.

--- cartoon.py
+++ cartoon.py
@@ -5,3 +5,12 @@ def gajurel(x, y):
 	turtle.setx(x)
 	turtle.sety(y)
 	print(x, y)
+
+class Cartoon:
+
+	def __init__(self):
+		self.t = turtle.Turtle()
+		t = self.t
+		t.pensize(3)
+		t.speed(9)
+		t.ondrag(gajurel)
